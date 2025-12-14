f379b8d7c198eb1cd8597ce995a23daf950d2364
RaviPithadiya <ravipithadiya316@gmail.com>
2025-12-14 14:28:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import weather from './Components/Weather';
+import Myapp from './Components/Myapp';
+
+function App() {
+  return (
+    <>
+     
+      <Myapp/>
+    </>
+  );
+}
+
+export default App;
